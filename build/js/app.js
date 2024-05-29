5c403a3a0c6fbd0bc7dd04c7acb584592339a0d0
Miguel martinez <maik_kb@live.com.mx>
2024-05-29 17:59:58 -0600

    termine la galeria y el efecto del header

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+    navegacionFija()
     crearGaleria()
 })
+
+function navegacionFija() {
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
+
+    //Agregamos un escucha del evento scroll
+    document.addEventListener('scroll', function () {
+        //ejecuta codigo si la cordenada actual es menor a 1 
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed')
+        } else {
+            header.classList.remove('fixed')
+        }
+
+    })
+
+}
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes') // Corregir este selector según corresponda
@@ -25,23 +43,37 @@ function mostrarImagen(i) {
     imagen.src = `src/img/gallery/full/${i}.jpg` // Utilizar comillas invertidas aquí
     imagen.alt = 'Imagen Galeria'
 
-    //General Modal
+    //Generar Modal
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
     modal.onclick = cerrarModal
+
+    //Boton cerrarModal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    //Agregar boton cerrar
+    modal.appendChild(cerrarModalBtn)
+
     //Agregar la imagen
-    modal.appendChild(imagen)
+    modal.insertBefore(imagen, cerrarModalBtn)
     //Agregar al Html
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
     body.appendChild(modal)
-
 }
 
 function cerrarModal() {
     const modal = document.querySelector('.modal')
     modal.classList.add('fadeOut')
+    const body = document.querySelector('body')
+
+
     setTimeout(() => {
         modal?.remove()
+        body.classList.remove('overflow-hidden')
 
     }, 500);
 
